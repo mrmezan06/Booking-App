f965713e8c23a100b684bdbd9f9d2ebc1bb19d91
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2022-09-27 01:34:36 +0600

    UPDATE HOTEL

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -13,6 +13,20 @@ router.post("/", async (req, res) => {
   }
 });
 // UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 // DELETE
 // GET
 // GET ALL
